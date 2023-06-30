2b212db326be056defd331067d2c44842a14b01d
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-30 14:48:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+let a = [{title : "Tab" , content : 1} , {title : "Tab" , content : 2} , {title : "Tab" , content : 3}];
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App a = {a}/>, document.getElementById("root"));
