421343f8ec8b26da482b8715c775dc6338727a37
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-30 14:47:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
+const App = ({a}) => {
+  let [change , setChange] = useState(0);
+  let [name ,setName] = useState("");
+  function change(e) {
+    setChange(e.content);
+    setName(e.title);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ul>
+          a.map((e)=> (
+            <li onClick={change(e)}>{a.title} {a.content}</li>
+          ))
+        </ul>
+        <p>This is the content for {name} {change}.</p>
     </div>
   )
 }
