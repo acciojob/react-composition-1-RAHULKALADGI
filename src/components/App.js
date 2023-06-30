6b3b2fca6f4343b234a6a7763e40b25588087a15
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-30 18:05:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,16 @@ import './../styles/App.css';
 let a = [{title : "Tab" , content : 1} , {title : "Tab" , content : 2} , {title : "Tab" , content : 3} ]
 
 const App = ()=> {
-   let [count , setCount] = useState();
+   let [count , setCount] = useState(1);
 
    return (
     <div>
         <ul>
-            <li onClick={()=>setCount(a[0].content)}>{a[0].title} {a[0].content}</li>
-            <li onClick={()=>setCount(a[1].content)}>{a[1].title} {a[1].content}</li>
-            <li onClick={()=>setCount(a[2].content)}>{a[2].title} {a[2].content}</li>
+            {
+                a.map((e)=> (
+                    <li onClick={()=>setCount(e.content)}>{e.title} {e.content}</li>
+                ))
+            }
         </ul>
         <p>This is the content for Tab {count}.</p>
     </div>
